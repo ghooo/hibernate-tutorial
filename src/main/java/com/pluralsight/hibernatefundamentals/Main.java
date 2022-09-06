cc011f9a9c0d7a7ec007628d3e6f073a044fc3f1
Mohamed Ghoneim <mohamed.ghoneim@airbnb.com>
2022-09-06 09:43:36 -0700

    showing 1-n relationship

--- src/main/java/com/pluralsight/hibernatefundamentals/Main.java
+++ src/main/java/com/pluralsight/hibernatefundamentals/Main.java
@@ -37,18 +37,17 @@ public class Main {
     ticket1.setNumber("1234");
     ticket1.setOrigin("Seattle");
     ticket1.setDestination("Cairo");
-
     ticket1.setPassenger(john);
-
-//    Ticket ticket2 = new Ticket(2, "BB5678");
-//    ticket2.setPassenger(john);
-
     john.addTicket(ticket1);
-//    john.addTicket(ticket2);
+
+    Ticket ticket2 = new Ticket();
+    ticket2.setSeries("BB");
+    ticket2.setNumber("5678");
+    ticket2.setPassenger(john);
+    john.addTicket(ticket2);
 
 //    Ticket ticket3 = new Ticket(3, "CC5678");
 //    ticket3.setPassenger(mike);
-
 //    mike.addTicket(ticket3);
 
     em.persist(airport);
@@ -57,7 +56,7 @@ public class Main {
     em.persist(mike);
 
     em.persist(ticket1);
-//    em.persist(ticket2);
+    em.persist(ticket2);
 //    em.persist(ticket3);
 
     em.getTransaction().commit();
